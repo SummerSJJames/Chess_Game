72c75e15a350dad0c7b4fc91f686153788ad3e94
Summer <sumxxlove@gmail.com>
2022-09-09 22:40:44 +0200

    Chess creation script

---
+++ Chess_Game/Assets/Code/BoardCreator.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardCreator : MonoBehaviour
+{
+    Transform[,] boardTiles = new Transform[8, 8];
+    GameObject quad;
+
+    Material white;
+    Material black;
+
+    public enum pieceType { FUCKINGEMPTYMATE, King, Queen, Pawn, Bishop, Knight, Rook };
+
+    private void Start()
+    {
+        quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        white = new Material(Shader.Find("Standard"));
+        black = new Material(Shader.Find("Standard"));
+        white.color = Color.white;
+        black.color = Color.black;
+        quad.GetComponent<Renderer>().material = new Material(Shader.Find("Standard"));
+        quad.transform.rotation = Quaternion.Euler(90, 0, 0);
+
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                quad.GetComponent<Renderer>().material = (i + j) % 2 == 0 ? white : black;
+
+                quad.transform.position = new Vector3(i, 0, j);
+                boardTiles[i, j] = quad.transform;
+                Instantiate(quad, quad.transform.position, quad.transform.rotation);
+            }
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+}
+
